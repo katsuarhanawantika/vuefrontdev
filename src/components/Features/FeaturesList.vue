0ecbc058c4c81d1bd92d9d758e7bf7168612e3c3
katsuar <katsuarhanawantika@gmail.com>
2023-01-06 23:42:01 +0700

    login register pemisahan komponen

---
+++ src/components/Features/FeaturesList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import CustomaizableVue from '@/components/Features/Customaizable.vue';
+import FortuneVue from '@/components/Features/Fortune.vue';
+import DocumentationVue from '@/components/Features/Documentation.vue';
+ 
+</script>
+
+<template>
+        <CustomaizableVue/>
+        <FortuneVue/>
+        <DocumentationVue/>
+</template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,34 @@
+
+<script setup>
+import { RouterLink } from 'vue-router';
+import FormComponentVue from '@/components/layouts/Authentication/FormComponent.vue'
+import FeatureList from '@/components/Features/FeaturesList.vue'
+</script>
+
+
+<template>
+  <div class="relative overflow-hidden bg-white">
+    <div class="mx-auto">
+      <div class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0">
+        <div class="w-full p-5 mx-auto md:max-w-4xl">
+          <h2 class="mb-10 text-5xl font-bold text-center">
+            <span class="block lg:mb-2">Join & Get Our</span>
+            <span class="block">Pro Benefits</span>
+          </h2>
+        </div>
+        <div class="w-full p-5 mx-auto md:max-w-4xl">
+          <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <div class="mx-auto md:mx-0">
+
+                <FeatureList/>
+
+            </div>
+            <div>
+              <FormComponentVue />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
