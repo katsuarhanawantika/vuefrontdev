6ce5787531ff4751d32ee367f0a299b1be50df52
katsuar <katsuarhanawantika@gmail.com>
2023-01-06 17:47:04 +0700

    pemecahan komponen navbar

---
+++ src/components/layouts/NavbarComp.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
+
+            <Logo/>
+            <NavigationLinks/>
+            <UserInf/>
+
+        </div>
+    </nav>
+</template>
+
+<script>
+ 
+import Logo from '@/components/layouts/NavbarItems/Logo.vue'
+import NavigationLinks from '@/components/layouts/NavbarItems/NavigationLinks.vue'
+import UserInf from '@/components/layouts/NavbarItems/UserInfo.vue'
+export default {
+ 
+    components:{
+        Logo,
+        NavigationLinks,
+        UserInf
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/layouts/NavbarItems/Logo.vue
@@ -0,0 +1,17 @@
+
+
+<template>
+              <RouterLink to="/" class="flex items-center">
+                <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Flowbite Logo" />
+            </RouterLink>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
