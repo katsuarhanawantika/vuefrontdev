dc8052e9b1336e77f497a5aee80f3177f26f98cf
katsuar <katsuarhanawantika@gmail.com>
2023-01-07 14:39:59 +0700

    hapus login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
+ 
 import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
@@ -10,12 +10,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
+    },     
     {
       path: '/register',
       name: 'register',
