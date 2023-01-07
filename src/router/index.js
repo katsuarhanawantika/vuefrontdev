83978f5b23f45d8d63cd142133e33339c9b009c5
katsuar <katsuarhanawantika@gmail.com>
2023-01-07 13:27:59 +0700

    penambahan login dan signup

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
-    },
+    }, 
     // {
     //   path: '/about',
     //   name: 'about',
